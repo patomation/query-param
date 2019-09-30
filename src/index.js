cfa537a42c8eba94831bd5ac890505cd86bd2b48
patomation <patomation@gmail.com>
2019-09-30 12:56:11 +0700

    Source module

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//Get query string from window object
+const queryString = window.location.search.substring(1)
+
+const queryParam = () => {
+  //Split by & and reduce array into object
+  return queryString.split('&').reduce( ( acc, item ) => {
+    //Split item pair by = into key and value
+    const [ key, value ] = item.split('=')
+    //If key and value add to object
+    if(key && value) acc[key] = value.replace('%20', ' ')
+    //return mutated object
+    return acc
+  }, {})
+}
+
+export default queryParam
